18b7e18aaeb69b8e2f2b347635b708fc2b0172cb
Allan Niles <allancoding.dev@gmail.com>
2026-05-29 11:05:14 -0600

    sdk 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "dev.allancoding.gospellibrary"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "dev.allancoding.gospellibrary"
